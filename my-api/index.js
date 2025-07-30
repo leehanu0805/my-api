aba299b278e48b0af1e453934d0db754f2283798
leehanu0805 <leehanu0805@gmail.com>
2025-07-31 01:03:58 +0200

    Update index.js

--- my-api/index.js
+++ my-api/index.js
@@ -1,11 +1,1 @@
-import express from "express";
 
-const app = express();
-
-app.get("/hello", (req, res) => {
-  res.json({ message: "Hello from API!" });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
